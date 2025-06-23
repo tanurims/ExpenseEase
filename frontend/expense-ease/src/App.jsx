0ebaf6c560a9a36d53caf24d7f6305ee1b01ad0f
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-23 14:34:03 +0530

    setup folder structure and routers

---
+++ frontend/expense-ease/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Expense from "./pages/Dashboard/Expense";
+import Income from "./pages/Dashboard/Income";
+
+const Root = ()=>{
+  //check if token is present in local storage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  //redirect to dashboard if authenticated,otherwise to login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp/>} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/income" element={<Income />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
