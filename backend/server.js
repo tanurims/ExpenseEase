34b9d929e8f624e0e60f60fc75be16f8bfce7896
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-24 22:54:39 +0530

    connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
+const connectDB = require('./config/db');
 const app = express();
 
 //middleware to handle cors
@@ -13,6 +13,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
