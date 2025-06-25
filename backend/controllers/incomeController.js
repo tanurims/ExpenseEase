90ed398beb585d0f81f29d281f3bf474cff541b7
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-26 00:47:08 +0530

    download excel api

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Income = require("../models/Income");
+const xlsx = require("xlsx");
 
 //add income 
 exports.addIncome = async (req, res) => {
@@ -66,5 +67,29 @@ exports.deleteIncome = async (req, res) => {
 
 //download excel 
 exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+
+        const income = await Income.find({userId}).sort({date: -1});
+
+        //prepare data for excel
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+        xlsx.writeFile(wb, "income_details.xlsx");
+        res.download("income_details.xlsx");
+
+        
+    } catch (error) {
+        res.status(500).json({message: "Server Error"});
+        
+    }
 
 }
