c95761bdc9f636dcf2226c11526171b2c81917ce
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-26 12:48:16 +0530

    dashboard api

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 const  authRoutes = require('./routes/authRoutes');
 const incomeRoutes = require('./routes/incomeRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
+const dashboarRoutes = require('./routes/dashboardRoutes');
 
 //middleware to handle cors
 app.use(cors({
@@ -22,6 +23,8 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboarRoutes);
+
 
 
 
